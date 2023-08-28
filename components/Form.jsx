e4ed12969292faa22fd9324b693152d63de354fa
Naome <tuyishimenaome27@gmail.com>
2023-08-29 00:05:30 +0300

    initial commit

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PromptCard from './PromptCard'
 
-const Form = () => {
+const Form = ({data,handleTagClick}) => {
   return (
     <div className='w-full max-w-full flex-start flex-col'>
-      Form
+      {data.map((post)=>{
+        <PromptCard 
+        key={post.id}
+        post={post}
+        handleTagClick={handleTagClick}
+        />
+      })}
     </div>
   )
+}
+
+const Feed= ()=> {
+  const [allPosts, setAllPosts] = useState([]);
+
+  //Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 }
 
 export default Form
