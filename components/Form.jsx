acf4ae595f6c741f6b6aea5fdf9af0cd46f71315
Naome <tuyishimenaome27@gmail.com>
2023-08-31 14:22:35 +0300

    final

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,38 +1,67 @@
-import React, { useState } from 'react'
-import PromptCard from './PromptCard'
+import Link from "next/link";
 
-const Form = ({data,handleTagClick}) => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div className='w-full max-w-full flex-start flex-col'>
-      {data.map((post)=>{
-        <PromptCard 
-        key={post.id}
-        post={post}
-        handleTagClick={handleTagClick}
-        />
-      })}
-    </div>
-  )
-}
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span>
+      </h1>
+      <p className='desc text-left max-w-md'>
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform
+      </p>
 
-const Feed= ()=> {
-  const [allPosts, setAllPosts] = useState([]);
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Your AI Prompt
+          </span>
 
-  //Search states
-  const [searchText, setSearchText] = useState("");
-  const [searchTimeout, setSearchTimeout] = useState(null);
-  const [searchedResults, setSearchedResults] = useState([]);
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder='Write your post here'
+            required
+            className='form_textarea '
+          />
+        </label>
 
-  const fetchPosts = async () => {
-    const response = await fetch("/api/prompt");
-    const data = await response.json();
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Field of Prompt{" "}
+            <span className='font-normal'>
+              (#product, #webdevelopment, #idea, etc.)
+            </span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            type='text'
+            placeholder='#Tag'
+            required
+            className='form_input'
+          />
+        </label>
 
-    setAllPosts(data);
-  };
+        <div className='flex-end mx-3 mb-5 gap-4'>
+          <Link href='/' className='text-gray-500 text-sm'>
+            Cancel
+          </Link>
 
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-}
+          <button
+            type='submit'
+            disabled={submitting}
+            className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'
+          >
+            {submitting ? `${type}ing...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
 
-export default Form
+export default Form;
