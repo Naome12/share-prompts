865c103841ac31de68ac7466de9824407fe90268
Naome <tuyishimenaome27@gmail.com>
2023-08-31 14:22:24 +0300

    final

--- app/profile/loading.jsx
+++ app/profile/loading.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import Image from "next/image";
 
-const loading = () => {
+const Loading = () => {
   return (
-    <div>
-      
+    <div className='w-full flex-center'>
+      <Image
+        src='assets/icons/loader.svg'
+        width={50}
+        height={50}
+        alt='loader'
+        className='object-contain'
+      />
     </div>
-  )
-}
+  );
+};
 
-export default loading
+export default Loading;
